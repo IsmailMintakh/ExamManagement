755f27e1c9993dc5696f8163b7488b5c282dc630
moslemparve <muslimparvi@gmail.com>
2026-05-16 23:45:31 +0500

    route fix hostinger

--- routes/web.php
+++ routes/web.php
@@ -480,5 +480,4 @@ Route::model('transfer', StudentTransfer::class);
 // Bind scheduling params
 Route::model('room', \App\Models\ExamRoom::class);
 Route::model('invigilator', \App\Models\ExamInvigilator::class);
-
 require __DIR__.'/auth.php';
